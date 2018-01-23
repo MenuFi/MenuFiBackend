8d08433bdef3338e52af6a54c5f92314f5086561
Charlie McKnight <ckmcknight@gmail.com>
2018-01-23 15:15:26 -0500

    Setting up SQL

--- src/main/java/com/menufi/backend/springboot/HelloworldController.java
+++ src/main/java/com/menufi/backend/springboot/HelloworldController.java
@@ -1,37 +1,23 @@
-/**
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.menufi.backend.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class HelloworldController {
-    Connection conn;
-    boolean initialized = false;
+    private Connection conn;
+    private boolean initialized = false;
 
     @GetMapping("/")
     public String hello() {
-        return response;
+        return sqlTest();
     }
 
-    public String sqlTest() {
+    private String sqlTest() {
         if (!initialized) {
             initConnection();
             initialized = true;
@@ -48,19 +34,24 @@ public class HelloworldController {
         return response;
     }
 
-    public void initConnection() {
+    private void initConnection() {
         try {
-            ApiProxy.Environment env = ApiProxy.getCurrentEnvironment();
-            Map<String,Object> attr = env.getAttributes();
-            String hostname = (String) attr.get("com.google.appengine.runtime.default_version_hostname");
-
-            String url = hostname.contains("localhost:")
-                    ? System.getProperty("cloudsql-local") : System.getProperty("cloudsql");
+            String url;
+            if (System.getProperty("com.google.appengine.runtime.version").startsWith("Google App Engine/")) {
+                // Check the System properties to determine if we are running on appengine or not
+                // Google App Engine sets a few system properties that will reliably be present on a remote
+                // instance.
+                url = System.getProperty("ae-cloudsql.cloudsql-database-url");
+                // Load the class that provides the new "jdbc:google:mysql://" prefix.
+                Class.forName("com.mysql.jdbc.GoogleDriver");
+            } else {
+                // Set the url with the local MySQL database connection url when running locally
+                url = System.getProperty("ae-cloudsql.local-database-url");
+            }
+            Logger.getGlobal().log(Level.SEVERE, url);
             conn = DriverManager.getConnection(url);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
-
     }
 }
